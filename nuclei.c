a1da2a2aadea067571137c5af49eeee1711cbb14
ABenC377 <84676155+ABenC377@users.noreply.github.com>
2022-12-14 19:58:14 +0000

    adding to lexical analyser functions

---
+++ nuclei.c
@@ -0,0 +1,170 @@
+int main(int argc, char* argv[]) {
+    
+    // Set up the file to be scanned
+    
+    Token_list* list = run_lexical_analyser(file_name);
+    
+}
+
+
+Token_list* run_lexical_analyser(char* file_name) {
+    FILE* fp = fopen(file_name, "r");
+    
+    // ungetc() is going to be an important function here!!!!
+    Token_list* tokens = get_tokens_from_file(fp);
+    
+    fclose(fp);
+    
+    return tokens;
+}
+
+Token_list* get_tokens_from_file(FILE* fp) {
+    Token_list* tokens = (Token_list*)allocate_space(1, sizeof(Token_list));
+    Automata* automata = (Automata*)allocate_space(1, sizeof(Automata));
+    char c = fgetc(fp);
+    while (c != EOF) {
+        update_tokens(tokens, automata, c);
+    }
+    free(automata);
+    return tokens;
+}
+
+void update_tokens(Token_list* tokens, Automata* automata, char c) {
+    switch (automata->state) {
+        case start:
+            handle_start_state(token, automata, c);
+            break;
+        case in_literal:
+            break;
+        case in_variable:
+            break;
+        case N:
+            break;
+        case NI:
+            break;
+        case W:
+            break;
+        case WH:
+            break;
+        case WHI:
+            break;
+        case WHIL:
+            break;
+        case I:
+            break;
+        case P:
+            break;
+        case PR:
+            break;
+        case PRI:
+            break;
+        case PRIN:
+            break;
+        case PL:
+            break;
+        case PLU:
+            break;
+        case S:
+            break;
+        case SE:
+            break;
+        case L:
+            break;
+        case LE:
+            break;
+        case LES:
+            break;
+        case LEN:
+            break;
+        case LENG:
+            break;
+        case LENGT:
+            break;
+        case G:
+            break;
+        case GR:
+            break;
+        case GRE:
+            break;
+        case GREA:
+            break;
+        case GREAT:
+            break;
+        case GREATE:
+            break;
+        case E:
+            break;
+        case EQ:
+            break;
+        case EQU:
+            break;
+        case EQUA:
+            break;
+        case C:
+            break;
+        case CA:
+            break;
+        case CD:
+            break;
+        case CO:
+            break;
+        case CON:
+            break;
+        default:
+            throw_error("ERROR: invalid automata state\n");
+    }
+}
+
+void handle_start_state(Token_list* tokens, Automata* automata, char c) {
+    switch (c) {
+        case ' ':
+            break;
+        case ')':
+            automata->token = (Token*)allocate_space(1, sizeof(Token));
+            automata->token->type = r_parenthesis;
+            add_token(tokens, automata->token);
+            break;
+        case '(':
+            automata->token = (Token*)allocate_space(1, sizeof(Token));
+            automata->token->type = l_parenthesis;
+            add_token(tokens, automata->token);
+            break;
+        case SINGLEQUOTE:
+            automata->token = (Token*)allocate_space(1, sizeof(Token));
+            automata->token->type = literal;
+            automata->token->literal_lexeme = (char*)allocate_space(LEXEMEMAXLEN, sizeof(char));
+            automata->state = in_literal;
+            break;
+        case '"':
+            automata->token = (Token*)allocate_space(1, sizeof(Token));
+            automata->token->type = string;
+            automata->token->string_lexeme = (char*)allocate_space(LEXEMEMAXLEN, sizeof(char));
+            automata->state = in_string;
+            break;
+        case 'A'...'Z':
+            if (c == 'C' || c == 'E' || c == 'G' || c == 'I' || c == 'L' || c == 'N' || c == 'P' || c == 'S' '|| c == W') {
+                
+            } else {
+                automata->token = (Token*)allocate_space(1, sizeof(Token));
+                automata->token->type = variable;
+                automata->token->var_name = c;
+                add_token(tokens, automata->token);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void* allocate_space(int num, int size) {
+    void* pointer = calloc(num, size);
+    if (!pointer) {
+        throw_error("ERROR: unable to allocate space\n");
+    }
+    return pointer;
+}
+
+void throw_error(const char* error_message) {
+    fputs(error_message, stderr);
+    exit(EXIT_FAILURE);
+}
--- nuclei.h
+++ nuclei.h
@@ -1,1 +1,69 @@
 #include "specific.h"
+
+#define SINGLEQUOTE 39
+#define LEXEMEMAXLEN 100000
+
+typedef enum {
+    literal, 
+    string, 
+    variable, 
+    nil, 
+    while, 
+    if, 
+    print, 
+    set,
+    equal,
+    greater,
+    less,
+    length,
+    plus,
+    CONS,
+    CDR,
+    CAR,
+    l_parenthesis, 
+    r_parenthesis
+} token_type;
+
+typedef enum {
+    start,
+    in_literal,
+    in_string,
+    N, NI,
+    W, WH, WHI, WHIL,
+    I,
+    P, PR, PRI, PRIN, PL, PLU,
+    S, SE,
+    L, LE, LES, LEN, LENG, LENGT,
+    G, GR, GRE, GREA, GREAT, GREATE,
+    E, EQ, EQU, EQUA,
+    C, CA, CD, CO, CON,
+} automata_state;
+
+typedef struct Cons {
+    struct Cons* car; // points to this element of the list
+    struct Cons* cdr; // points to the remainder of the list
+    atomtype value;
+} Cons;
+
+typedef struct {
+    token_type type;
+    char* string_lexeme;
+    char* literal_lexeme;
+    char var_name;
+} Token;
+
+typedef struct Token_node {
+    Token* value;
+    struct Token_node* next;
+} Token_node;
+
+typedef struct Token_list {
+    Token_node* start;
+    Token_node* end;
+} Token_list;
+
+typedef struct {
+    automata_state state;
+    Token* token;
+} Automata;
+
