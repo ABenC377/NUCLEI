32bf635f62e4e0dc39cdf4d78cc99b1de6a50aab
ABenC377 <84676155+ABenC377@users.noreply.github.com>
2022-12-18 18:04:08 +0000

    fixing parsing bug

--- parse.c
+++ parse.c
@@ -2,14 +2,15 @@
 
 void parse_list(Token_list* list) {
     if (is_prog(list->start)) {
-        printf("Parse: yes\n");
+        printf("Parsed OK\n");
     } else {
-        printf("Parse: no\n");
+        exit(EXIT_FAILURE);
     }
 }
 
 bool is_prog(Token_node* start) {
     if (start->value->type != t_l_parenthesis) {
+        printf("No oen parenthis\n"); // debugging
         return false;
     } else {
         return is_instrcts(&(start->next));
@@ -23,6 +24,7 @@ bool is_instrcts(Token_node** start) {
         return true;
     } else {
         if (!is_instrct(&start_node)) {
+            printf("No instruction in instructions\n"); // debugging
             return false;
         } else {
             return is_instrcts(&start_node);
@@ -33,13 +35,16 @@ bool is_instrcts(Token_node** start) {
 bool is_instrct(Token_node** start) {
     Token_node* start_node = *start;
     if (start_node->value->type != t_l_parenthesis) {
+        printf("No open parenthesis in instruction\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (!is_func(&start_node)) {
+        printf("No function in instruction\n"); // debugging
         return false;
     }
     if (start_node->value->type != t_r_parenthesis) {
+        printf("No closing parenthesis in instruction\n"); // debugging
         return false;
     } else {
         *start = start_node->next;
@@ -78,6 +83,7 @@ bool is_listfunc(Token_node** start) {
             }
         }
     }
+    printf("No list function\n"); // debugging
     return false;
 }
 
@@ -98,6 +104,7 @@ bool is_intfunc(Token_node** start) {
             return true;
         }
     }
+    printf("No int function\n"); // debugging
     return false;
 }
 
@@ -128,6 +135,7 @@ bool is_boolfunc(Token_node** start) {
             }
         }
     }
+    printf("No bool function\n"); // debugging
     return false;
 }
 
@@ -147,6 +155,7 @@ bool is_set(Token_node** start) {
             }
         }
     }
+    printf("No set\n"); // debugging
     return false;
 }
 
@@ -163,38 +172,47 @@ bool is_print(Token_node** start) {
             return true;
         }
     }
+    printf("No print\n"); // debugging
     return false;
 }
 
 bool is_if(Token_node** start) {
     Token_node* start_node = *start;
     if (start_node->value->type != t_if) {
+        printf("No if statement in if\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (start_node->value->type != t_l_parenthesis) {
+        printf("No open parenthesis in if\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (!is_boolfunc(&start_node)) {
+        printf("No bool function in if\n"); // debugging
         return false;
     }
     if (start_node->value->type != t_r_parenthesis) {
+        printf("No close parenthesis in if\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (start_node->value->type != t_l_parenthesis) {
+        printf("No open parenthesis in if\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (!is_instrcts(&start_node)) {
+        printf("No first instructions in if\n"); // debugging
         return false;
     }
     if (start_node->value->type != t_l_parenthesis) {
+        printf("No open parenthesis in if\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (!is_instrcts(&start_node)) {
+        printf("No second instructions in if\n"); // debugging
         return false;
     }
     *start = start_node;
@@ -204,25 +222,31 @@ bool is_if(Token_node** start) {
 bool is_loop(Token_node** start) {
     Token_node* start_node = *start;
     if (start_node->value->type != t_while) {
+        printf("No while statement in loop\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (start_node->value->type != t_l_parenthesis) {
+        printf("No open parenthesisin loop\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (!is_boolfunc(&start_node)) {
+        printf("No bool function in loop\n"); // debugging
         return false;
     }
     if (start_node->value->type != t_r_parenthesis) {
+        printf("No close parenthesis in loop\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (start_node->value->type != t_l_parenthesis) {
+        printf("No open parenthesis in loop\n"); // debugging
         return false;
     }
     start_node = start_node->next;
     if (!is_instrcts(&start_node)) {
+        printf("No instructions in loop\n"); // debugging
         return false;
     }
     *start = start_node;
@@ -243,14 +267,17 @@ bool is_list(Token_node** start) {
     } else if (start_node->value->type == t_l_parenthesis) {
         start_node = start_node->next;
         if (!is_retfunc(&start_node)) {
+        printf("No open parenthesis in list\n"); // debugging
             return false;
         }
         if (start_node->value->type != t_r_parenthesis) {
+        printf("No close parenthesis in list\n"); // debugging
             return false;
         }
         start_node = start_node->next;
         *start = start_node;
     }
+    printf("No list\n");
     return false;
 }
 
